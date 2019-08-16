fe652bb43b7f7dc29df47b8db289c9d8d92681a8
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-16 02:00:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
-  if speak_to_grandma("I LOVE YOU GRANDMA!")
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-    elsif speak_to_grandma(phrase.upcase)
+    elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP SONNY!"
